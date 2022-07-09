44ee81e47ffe424c365161e4a2a37d852af84010
shford <stevenhford3@gmail.com>
2022-07-09 17:44:00 -0600

    minor formatting fix

--- rw_test.cu
+++ rw_test.cu
@@ -13,7 +13,7 @@
 
 
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort=true)
 {
     if (code != cudaSuccess)
     {
