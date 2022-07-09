2665271fa999dd847c9307ccfebb321da1de1631
shford <stevenhford3@gmail.com>
2022-07-09 17:44:33 -0600

    restructured. gpuassert err line 78.

--- cuda_scoping.cu
+++ cuda_scoping.cu
@@ -9,7 +9,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-//#include <fileapi.h>
+#include "cuda_consts.cuh"
 
 
 #define TARGET_COLLISIONS (5)
@@ -17,7 +17,7 @@
 
 // globally accessible device variables (L2 cache on CUDA 8.6)
 __device__ volatile char* collision;
-__device__ bool terminate_signal;
+__device__ bool volatile terminate_signal = false;
 
 
 __global__ void kernel()
@@ -35,69 +35,69 @@ __global__ void kernel()
         bool hashes_match = true;
         if (hashes_match == true)
         {
-            // write local data to global for host polling
-            cudaMemcpyAsync((void*)collision, local_data, ARBITRARY_MAX_DATA_SIZE, cudaMemcpyDeviceToDevice, 0);
-
-            // free local_data
-            cudaFree(local_data);
-
             // update global flag for host polling flag
             terminate_signal = true;
+
+            // write local data to global for host polling
+            cudaMemcpyAsync((void*)collision, local_data, ARBITRARY_MAX_DATA_SIZE, cudaMemcpyDeviceToDevice, 0);
         }
     }
+    cudaFree(local_data);
 }
 
 int main()
 {
-    // sample append data
-    char* data = (char*)calloc(1, ARBITRARY_MAX_DATA_SIZE);
-    sprintf_s(data, ARBITRARY_MAX_DATA_SIZE, "%s", "test");
+    // allocate host local mem for original data
+    char* page_locked_host_data;
+    gpuErrchk( cudaMallocHost(&page_locked_host_data, ARBITRARY_MAX_DATA_SIZE) );
+    sprintf_s(page_locked_host_data, ARBITRARY_MAX_DATA_SIZE, "test");
 
-    // flag true if collision is found
-    bool collision_found = false;
-
-    // allocate mem for collision
-    cudaMalloc(&collision, ARBITRARY_MAX_DATA_SIZE);
+    // allocate global mem for collision
+    gpuErrchk( cudaMalloc(&collision, ARBITRARY_MAX_DATA_SIZE) );
 
     // copy data to collision in global device memory
-    cudaMemcpyToSymbol(&collision, data, strlen(data)+1, 0, cudaMemcpyHostToDevice);
-
-    // set terminate_signal to false in global device memory
-    cudaMemcpyToSymbol(&terminate_signal, &collision_found, sizeof(collision_found), 0, cudaMemcpyHostToDevice);
+    gpuErrchk( cudaMemcpyToSymbol(collision, page_locked_host_data, strlen(page_locked_host_data)+1, 0, cudaMemcpyHostToDevice) );
 
     // hash until 5 hashes are found
     int collision_count = 0;
+    bool collision_found = false;
     while (collision_count != TARGET_COLLISIONS)
     {
         // execution configuration (sync device)
-        kernel<<<1, 32>>>();
+        kernel<<<1, 1>>>();
 
-        // read collision status from device todo check ptr issues in call
-        cudaMemcpyFromSymbol(&collision_found, &terminate_signal, sizeof(collision_found), 0, cudaMemcpyDeviceToHost);
+//        printf(collision_found ? "true\n" : "false\n");
 
         // poll collision flag
-        while (collision_found != true)
-        {
-            // read the reported value (strlen fails on volatile - either loop or copy the whole thing)
-            cudaMemcpyFromSymbol(&data, collision, ARBITRARY_MAX_DATA_SIZE, 0, cudaMemcpyDeviceToHost);
+        do {
+            // read collision status from device into host flag
+            gpuErrchk( cudaMemcpyFromSymbol(&collision_found, terminate_signal, sizeof(collision_found), 0, cudaMemcpyDeviceToHost) );
 
-            // check hashes match
-            bool hashes_match = true;
-            if (hashes_match == true)
-            {
-                // write file (note this should be multi-threaded. this is a waste of time when the gpu is likely idling)
+            if (collision_found) {
+                // read the reported value (strlen fails on volatile - either loop or copy the whole thing)
+                gpuErrchk( cudaMemcpyFromSymbol((void*)page_locked_host_data, *collision, ARBITRARY_MAX_DATA_SIZE, 0, cudaMemcpyDeviceToHost) );
 
-                // increase collision count
-                ++collision_count;
+                // ensure hashes match in case
+                if (true) // todo replace true w/ 20 bit comparisons
+                {
+                    // write file (note this should be multi-threaded. this is a waste of time when the gpu is likely idling)
+
+                    // increase collision count
+                    ++collision_count;
+
+                    // reset collision_found and terminate_flag
+                    collision_found = false;
+                    gpuErrchk( cudaMemcpyToSymbol(terminate_signal, &collision_found, sizeof(collision_found), 0, cudaMemcpyHostToDevice) );
+                }
             }
-        }
+        } while(!collision_found);
 
         // ensure kernel processes are finished
         cudaDeviceSynchronize();
     }
 
+    cudaFree(page_locked_host_data);
     cudaFree((void*)collision);
-    free(data);
 
     printf("Everything worked... /");
 }
