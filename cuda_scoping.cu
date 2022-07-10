5c25a19adf64d6b1d76864309245f9d582dfb8e6
shford <stevenhford3@gmail.com>
2022-07-09 22:42:31 -0600

    close. figure out why str[0] won't update.

--- cuda_scoping.cu
+++ cuda_scoping.cu
@@ -8,16 +8,15 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include "cuda_consts.cuh"
 
 
 #define TARGET_COLLISIONS (5)
 #define ARBITRARY_MAX_DATA_SIZE (1024)
 
-// globally accessible device variables (L2 cache on CUDA 8.6)
+
 __device__ volatile char* collision;
-__device__ bool volatile terminate_signal = false;
+__device__ volatile bool terminate_signal = false;
 
 
 __global__ void kernel()
@@ -28,6 +27,12 @@ __global__ void kernel()
 
     // copy global data to local
     cudaMemcpyAsync(local_data, (const void*)collision, ARBITRARY_MAX_DATA_SIZE, cudaMemcpyDeviceToDevice, 0);
+
+    // modify local data
+//    local_data[0] = '9';
+    collision[0] = '9';
+    //    local_data[7] = '8';
+    //    local_data[8] = '\0';
 
     while (terminate_signal == false)
     {
@@ -47,35 +52,44 @@ __global__ void kernel()
 
 int main()
 {
-    // allocate host local mem for original data
+    // allocate page-locked host variables
     char* page_locked_host_data;
     gpuErrchk( cudaMallocHost(&page_locked_host_data, ARBITRARY_MAX_DATA_SIZE) );
-    sprintf_s(page_locked_host_data, ARBITRARY_MAX_DATA_SIZE, "test");
+    cudaMemset(&page_locked_host_data, 0x00, sizeof(char)*ARBITRARY_MAX_DATA_SIZE);
+    strncpy_s(page_locked_host_data, strlen("1234567")+1, "1234567", ARBITRARY_MAX_DATA_SIZE);
+
+    bool* collision_found;
+    gpuErrchk( cudaMallocHost(&collision_found, sizeof(bool)) );
+    *collision_found = false;
 
     // allocate global mem for collision
     gpuErrchk( cudaMalloc(&collision, ARBITRARY_MAX_DATA_SIZE) );
+    gpuErrchk( cudaMemset((void*)collision, 0x00, sizeof(char)*ARBITRARY_MAX_DATA_SIZE) );
 
     // copy data to collision in global device memory
-    gpuErrchk( cudaMemcpyToSymbol(collision, page_locked_host_data, strlen(page_locked_host_data)+1, 0, cudaMemcpyHostToDevice) );
+    gpuErrchk( cudaMemcpyToSymbol(collision, (const void*)page_locked_host_data, strlen(page_locked_host_data)+1, 0, cudaMemcpyHostToDevice) );
 
     // hash until 5 hashes are found
     int collision_count = 0;
-    bool collision_found = false;
     while (collision_count != TARGET_COLLISIONS)
     {
         // execution configuration (sync device)
         kernel<<<1, 1>>>();
 
-//        printf(collision_found ? "true\n" : "false\n");
-
         // poll collision flag
         do {
             // read collision status from device into host flag
-            gpuErrchk( cudaMemcpyFromSymbol(&collision_found, terminate_signal, sizeof(collision_found), 0, cudaMemcpyDeviceToHost) );
-
+            cudaMemcpyFromSymbol(collision_found, terminate_signal, sizeof(collision_found));
+            printf(collision_found ? "true\n" : "false\n");
             if (collision_found) {
-                // read the reported value (strlen fails on volatile - either loop or copy the whole thing)
-                gpuErrchk( cudaMemcpyFromSymbol((void*)page_locked_host_data, *collision, ARBITRARY_MAX_DATA_SIZE, 0, cudaMemcpyDeviceToHost) );
+                // todo read the reported value (strlen fails on volatile - either loop or copy the whole thing)
+                 cudaMemcpyFromSymbol((void*)page_locked_host_data, collision, strlen(page_locked_host_data)+1, 0, cudaMemcpyDeviceToHost);
+//                for (int i = 0; i < 5; ++i) {
+//                    char byte[10];
+//                    cudaMemcpyFromSymbol(byte, collision, sizeof(char)*9);
+//                    int tmp = 2;
+//                  page_locked_host_data[i] = byte;
+//                }
 
                 // ensure hashes match in case
                 if (true) // todo replace true w/ 20 bit comparisons
@@ -86,8 +100,8 @@ int main()
                     ++collision_count;
 
                     // reset collision_found and terminate_flag
-                    collision_found = false;
-                    gpuErrchk( cudaMemcpyToSymbol(terminate_signal, &collision_found, sizeof(collision_found), 0, cudaMemcpyHostToDevice) );
+                    *collision_found = false;
+                    cudaMemcpyToSymbol(terminate_signal, collision_found, sizeof(collision_found), 0, cudaMemcpyHostToDevice);
                 }
             }
         } while(!collision_found);
@@ -96,8 +110,11 @@ int main()
         cudaDeviceSynchronize();
     }
 
-    cudaFree(page_locked_host_data);
-    cudaFree((void*)collision);
+    printf("Everything worked... /\n");
+    printf("%c\n", page_locked_host_data[0]);
+    printf("Modified string: %s\n", page_locked_host_data);
 
-    printf("Everything worked... /");
+    cudaFreeHost(page_locked_host_data);
+    cudaFreeHost(collision_found);
+    cudaFree((void*)collision);
 }
