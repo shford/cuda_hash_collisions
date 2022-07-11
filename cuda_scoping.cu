e7718a1bed8deec5d3436068d3f557c4b8443ae5
shford <stevenhford3@gmail.com>
2022-07-11 10:36:44 -0600

    appears to working. no errors, warnings, or bugs.

--- cuda_scoping.cu
+++ cuda_scoping.cu
@@ -22,126 +22,109 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
     }
 }
 
-
 #define TARGET_COLLISIONS (5)
 #define ARBITRARY_MAX_BUFF_SIZE (1024)
 #define FALSE (0)
 #define TRUE (1)
 
 
-__device__ char* collision;
-__device__ unsigned long long collision_cstring_size;
-__device__ int read_collision = FALSE;
-__device__ int terminate_signal = FALSE;
+__device__ int d_collision_flag = FALSE;
+__device__ unsigned long long d_collision_cstring_size;
+//__device__ int d_terminate_kernel = FALSE;
 
-__global__ void kernel(unsigned long long page_locked_host_data_cstring_size)
+__global__ void kernel(char* collision, unsigned long long page_locked_host_data_cstring_size)
 {
     // initialize local data
-    char *local_data;
-    cudaMalloc(&local_data, ARBITRARY_MAX_BUFF_SIZE);
+    char local_data[ARBITRARY_MAX_BUFF_SIZE];
 
     // copy global data to local
-    collision_cstring_size = page_locked_host_data_cstring_size;
-    cudaMemcpyAsync(local_data, collision, collision_cstring_size, cudaMemcpyDeviceToDevice, 0);
+    d_collision_cstring_size = page_locked_host_data_cstring_size;
+    for (int byte_index = 0; byte_index <= d_collision_cstring_size; ++byte_index) {
+        local_data[byte_index] = collision[byte_index];
+    }
+    //cudaMemcpyAsync(local_data, collision, d_collision_cstring_size, cudaMemcpyDeviceToDevice, 0);
 
     // modify local data to test copying back to global
-    if (collision_cstring_size + 1 < ARBITRARY_MAX_BUFF_SIZE) {
-        local_data[collision_cstring_size - 1] = '8';
-        local_data[collision_cstring_size] = '\0';
-        ++collision_cstring_size;
-    }
+//    if (d_collision_cstring_size + 1 < ARBITRARY_MAX_BUFF_SIZE) {
+        local_data[d_collision_cstring_size - 2] = '6';
+        local_data[d_collision_cstring_size - 1] = '7';
+        local_data[d_collision_cstring_size] = '\0';
+        ++d_collision_cstring_size;
+//    }
 
     // replace condition to ensure hashes match
     if (true)
     {
         // write local data to global for host polling
-        cudaMemcpyAsync(collision, local_data, collision_cstring_size, cudaMemcpyDeviceToDevice, 0);
-
-        // free local data
-        cudaFree(local_data);
+        for (int byte_index = 0; byte_index <= d_collision_cstring_size; ++byte_index) {
+            collision[byte_index] = local_data[byte_index];
+        }
+//        cudaMemcpyAsync(collision, local_data, d_collision_cstring_size, cudaMemcpyDeviceToDevice, 0);
 
         // tell host to read collision
-        read_collision = TRUE;
+        d_collision_flag = TRUE;
     }
 
-    // unsure if necessary - may be wholely unneeded or may could be deleted by applying volatile attr to collision
+    // unsure if necessary - may be wholly unneeded or may could be deleted by applying volatile attr to collision
     // in theory do not release kernel level (L2) memory until host confirms it's been read
-    int idling = 0;
-    while (!terminate_signal)
-    {
-        ++idling; // this line is only here to keep the compiler from optimizing the busy loop away
-    }
+//    int idling = 0;
+//    while (!d_terminate_kernel)
+//    {
+//        ++idling; // this line is only here to keep the compiler from optimizing the busy loop away
+//    }
 }
 
 int main()
 {
+    // collision storage
+    char* h_collisions[TARGET_COLLISIONS];
+    for (int i = 0; i < TARGET_COLLISIONS; ++i) {
+        h_collisions[i] = (char*)calloc(1, ARBITRARY_MAX_BUFF_SIZE);
+    }
+//    char h_collisions[TARGET_COLLISIONS][ARBITRARY_MAX_BUFF_SIZE];
+    unsigned long long h_collision_sizes[TARGET_COLLISIONS];
+
     // existing base data
-    char tmp_filler_base_data[] = "1234567";
-    unsigned long long page_locked_host_data_cstring_size = strlen(tmp_filler_base_data) + 1; // adjust for null terminator
+    char tmp_filler_base_data[] = "0123456";
+    unsigned long long h_page_locked_data_cstring_size = strlen(tmp_filler_base_data) + 1; // adjust for null terminator
 
     // allocate and initialize page-locked host variables
-    char* page_locked_host_data;
-    gpuErrchk( cudaMallocHost(&page_locked_host_data, ARBITRARY_MAX_BUFF_SIZE) );
-    cudaMemset(&page_locked_host_data, 0x00, sizeof(char) * ARBITRARY_MAX_BUFF_SIZE);
-    strncpy_s(page_locked_host_data, page_locked_host_data_cstring_size, tmp_filler_base_data, ARBITRARY_MAX_BUFF_SIZE);
-
-    int* collision_found;
-    gpuErrchk( cudaMallocHost((void**)&collision_found, sizeof(int)) ); //todo CHECK THIS LINE w/ and w/out &
-    *collision_found = FALSE;
+    char* h_page_locked_data;
+    gpuErrchk( cudaMallocHost(&h_page_locked_data, ARBITRARY_MAX_BUFF_SIZE) );
+    cudaMemset(&h_page_locked_data, 0x00, sizeof(char) * ARBITRARY_MAX_BUFF_SIZE);
+    strncpy_s(h_page_locked_data, h_page_locked_data_cstring_size, tmp_filler_base_data, ARBITRARY_MAX_BUFF_SIZE);
 
     // allocate global mem for collision
-    gpuErrchk( cudaMalloc((void**)&collision, ARBITRARY_MAX_BUFF_SIZE) ); //todo CHECK THIS LINE w/ and w/out &
-    gpuErrchk( cudaMemset(collision, 0x00, sizeof(char) * ARBITRARY_MAX_BUFF_SIZE) );
-
-    // copy data to collision in global device memory
-    gpuErrchk( cudaMemcpyToSymbol(collision, page_locked_host_data, page_locked_host_data_cstring_size, 0, cudaMemcpyHostToDevice) );
+    char* d_collision;
+    gpuErrchk( cudaMalloc((void **)&d_collision, ARBITRARY_MAX_BUFF_SIZE) );
+    cudaMemcpy(d_collision, h_page_locked_data, h_page_locked_data_cstring_size, cudaMemcpyHostToDevice);
 
     // hash until 5 hashes are found
     int collision_count = 0;
+    int h_collision_found = FALSE;
     while (collision_count != TARGET_COLLISIONS)
     {
-        printf("Reached before kernel.\n");
         // execution configuration (sync device)
-        kernel<<<1, 1>>>(page_locked_host_data_cstring_size);
-        printf("Reached after kernel.\n");
-
-        /*
-         * ensure we're not trying to read from global memory before
-         * the kernel can reserve/allocate it
-         */
-        int wait_cycles = 0;
-        while (wait_cycles < 1000)
-        {
-            ++wait_cycles;
-        }
+        h_collision_found = FALSE;
+        kernel<<<3, 128>>>(d_collision, h_page_locked_data_cstring_size);
 
         // poll collision flag
-        do {
-            const void* dev_N;
-            size_t symbolSize;
-            gpuErrchk( cudaGetSymbolSize(&symbolSize, read_collision) );
-            gpuErrchk( cudaGetSymbolAddress((void**)&dev_N, read_collision) );
-            gpuErrchk( cudaMemcpy((void*)collision_found, dev_N, symbolSize, cudaMemcpyDeviceToHost) );
+        while (!h_collision_found || collision_count != TARGET_COLLISIONS)
+        {
             // read collision status from device into host flag
-//            gpuErrchk( cudaMemcpyFromSymbol(collision_found, read_collision, sizeof(*collision_found), 0, cudaMemcpyDeviceToHost) );
-            printf("collision_found = %s after read.", *collision_found ? "TRUE" : "FALSE");
+            gpuErrchk( cudaMemcpyFromSymbol(&h_collision_found, d_collision_flag, sizeof(h_collision_found), 0, cudaMemcpyDeviceToHost) );
+            printf("h_collision_found = %s after read.\n", h_collision_found ? "TRUE" : "FALSE");
 
-            if (*collision_found) {
-                // read updated collision size into page_locked_host_data size
-                gpuErrchk(cudaMemcpyFromSymbol(&page_locked_host_data_cstring_size, collision_cstring_size, sizeof(page_locked_host_data_cstring_size), 0, cudaMemcpyDeviceToHost) );
+            if (h_collision_found)
+            {
+                // read updated collision size into h_page_locked_data size
+                gpuErrchk(cudaMemcpyFromSymbol(&h_collision_sizes[collision_count], d_collision_cstring_size, sizeof(h_page_locked_data_cstring_size), 0, cudaMemcpyDeviceToHost) );
 
                 // read from collision to host mem
-                gpuErrchk( cudaMemcpyFromSymbol(page_locked_host_data, collision, page_locked_host_data_cstring_size, 0, cudaMemcpyDeviceToHost) );
+                gpuErrchk( cudaMemcpy(h_collisions[collision_count], d_collision, h_page_locked_data_cstring_size, cudaMemcpyDeviceToHost) );
 
-                // for (int i = 0; i < 5; ++i) {
-                // char byte[10];
-                // cudaMemcpyFromSymbol(byte, collision, sizeof(char)*9);
-                // int tmp = 2;
-                // page_locked_host_data[i] = byte;
-                // }
-
-                // tell gpu threads to exit when they next read terminate_signal
-                gpuErrchk( cudaMemcpyToSymbol(terminate_signal, collision_found, sizeof(*collision_found), 0, cudaMemcpyHostToDevice) );
+                // tell gpu threads to exit when they next read d_terminate_kernel
+//                gpuErrchk( cudaMemcpyToSymbol(d_terminate_kernel, &h_collision_found, sizeof(h_collision_found), 0, cudaMemcpyHostToDevice) );
 
                 // replace condition with hash check
                 if (true) // todo replace true w/ 20 bit comparisons
@@ -151,23 +134,22 @@ int main()
                     // increase collision count
                     ++collision_count;
 
-                    // reset collision_found, terminate_flag, and read_collision
-                    *collision_found = FALSE;
-                    gpuErrchk( cudaMemcpyToSymbol(terminate_signal, collision_found, sizeof(*collision_found), 0, cudaMemcpyHostToDevice) );
-                    gpuErrchk( cudaMemcpyToSymbol(read_collision, collision_found, sizeof(*collision_found), 0, cudaMemcpyHostToDevice) );
+                    // ensure kernel processes are finished
+                    gpuErrchk( cudaDeviceSynchronize() );
                 }
             }
-        } while(!(*collision_found));
-
-        // ensure kernel processes are finished
-        cudaDeviceSynchronize();
+        }
     }
 
-    printf("Success... /\n");
-    printf("The kernel changed the following character: %c\n", page_locked_host_data[0]);
-    printf("Full modified string: %s\n", page_locked_host_data);
+    printf("\nSuccess... /\n");
+    printf("Original string: %s\n", h_page_locked_data);
+    for (int i = 0; i < collision_count; ++i) {
+        printf("Collision %d: %s\n", i, h_collisions[i]);
+    }
 
-    cudaFreeHost(page_locked_host_data);
-    cudaFreeHost(collision_found);
-    cudaFree((void*)collision);
+    cudaFreeHost(h_page_locked_data);
+    cudaFree(d_collision);
+    for (int i = 0; i < TARGET_COLLISIONS; ++i) {
+        free(h_collisions[i]);
+    }
 }
