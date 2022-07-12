a845b385a83c90f94c0d5554849e46f6bb820508
shford <stevenhford3@gmail.com>
2022-07-12 13:19:25 -0600

    commented out macro

--- main.cu
+++ main.cu
@@ -1,7 +1,9 @@
 /*
- * Author:  Hampton Ford
- * Github:  @shford
- * Status: Incomplete
+ * Author:      Hampton Ford
+ * Github:      @shford
+ * Status:      Incomplete
+ *
+ * Notes:       Max file size set at 4GiB b/c of Crypto Library limitations
  *
  * License:
  *
@@ -14,51 +16,22 @@
  *
  */
 
-#include <iostream>
-#include <math.h>
+#include "tasks.cuh"
 
-#include "subroutines.h"
-#include "WjCryptLib_Md5.h"
-
-#define CUDA_API_PER_THREAD_DEFAULT_STREAM
-#include <cuda_runtime_api.h>
-#include <cuda.h>
-#include "cuda_consts.cuh"
+//#define CUDA_API_PER_THREAD_DEFAULT_STREAM
 
 /*
  * todo
  * set shared memory capacity to 0 - cudaFuncSetAttribute(kernel_name, cudaFuncAttributePreferredSharedMemoryCarveout, 0);
+ *
+ * V5:
+ *  Multi-Thread/Multi-Process Initial & Final File I/O
+ *  Multi-Thread/Multi-Process Task 2
  */
 
-// group initial non-parallel tasks for neatness
-__host__ int initial_sequential_tasks(char** sample_file_buff, long* sample_buff_size, MD5_HASH* md5_digest)
-{
-    // read file data
-    char sample_file_path[] = "C:/Users/C22Steven.Ford/Downloads/Fall 2022/CyS 431 - Crypto/PEX 2/samplefile.txt";
-    get_file_data(sample_file_path, sample_file_buff, sample_buff_size);
-
-    // get hash md5_digest: 5442f94666075ef8d695109af238a5db
-    Md5Calculate_host(*sample_file_buff, (uint32_t)strlen(*sample_file_buff), md5_digest);
-
-    // format and print digest as a string of hex characters
-    char hash[MD5_HASH_SIZE_B + 1]; //MD5 len is 16B, 1B = 2 chars
-
-    char tiny_hash[TINY_HASH_SIZE_B + 1];
-    for (int i = 0; i < MD5_HASH_SIZE / 2; ++i)
-    {
-        sprintf(hash + i * 2, "%2.2x", (*md5_digest).bytes[i]);
-    }
-    hash[sizeof(hash)-1] = '\0';
-    strncpy_s(tiny_hash, sizeof(tiny_hash), hash, _TRUNCATE);
-    tiny_hash[sizeof(tiny_hash)-1] = '\0';
-
-    printf("Full MD5 md5_digest is: %s\n", hash);
-    printf("TinyHash md5_digest is: %s\n\n", tiny_hash);
-
-    return 0;
-}
 
 // resize cuda buffer
+/*
 __device__ void cuda_resize_buff(char** parent_scope_buff_dev, long src_buff_size_dev, long dst_buff_size_dev)
 {
     char* tmp_src_buff_dev;
@@ -75,10 +48,11 @@ __device__ void cuda_resize_buff(char** parent_scope_buff_dev, long src_buff_siz
     // free original buffer
     cudaFree(tmp_src_buff_dev);
 }
+*/
 
 // kernel
-__global__
-void find_collisions(__device__ __constant__ MD5_HASH original_md5_hash, const char* original_buff_host, const int original_buff_size_host, int* unsafe_collision_counter_host, unsigned long long* unsafe_hash_attempts_host)
+/*
+__global__ void find_collisions(__device__ __constant__ MD5_HASH original_md5_hash, const char* original_buff_host, const int original_buff_size_host, int* unsafe_collision_counter_host, unsigned long long* unsafe_hash_attempts_host)
 {
     // allocate maximum unified memory per thread (gpu space inefficient, but prevents costly buffer resizing)
     char* new_buff_dev;
@@ -138,29 +112,13 @@ void find_collisions(__device__ __constant__ MD5_HASH original_md5_hash, const c
     // free unified memory - leave original buffer RO b/c it's shared w/ other blocks
     cudaFree(new_buff_dev);
 }
+*/
 
 
 int main()
 {
     printf("Hash Collider - Starting Task 1...");
-
-    // execute sequential tasks
-    char* sample_buff;
-    long sample_buff_size;
-    MD5_HASH md5_digest;
-    initial_sequential_tasks(&sample_buff, &sample_buff_size, &md5_digest);
-
-    // 4864 cores/kernels/threads - max number of concurrent kernels possible at time
-    int unsafe_collision_counter_host;
-    unsigned long long unsafe_hash_attempts_host;
-    find_collisions<<<multiprocessors, cuda_cores_per_multiprocessor>>>(md5_digest, sample_buff, sample_buff_size, &unsafe_collision_counter_host, &unsafe_hash_attempts_host);
-
-    // wait for GPU to finish before accessing on host
-    gpuErrchk( cudaPeekAtLastError() );
-    gpuErrchk( cudaDeviceSynchronize() );
-
-    // free host memory
-    free(sample_buff);
+    task1();
 
     return 0;
 }
